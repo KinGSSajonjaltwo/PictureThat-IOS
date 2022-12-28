faa15b2ea982d2d8b00f7a25c8b182b7de97c630
minsop <minsoplee2015@gmail.com>
2022-12-28 21:14:31 +0900

    MainViewController WKWebView 추가
    mainWebView.load(urlReq)

---
+++ PictureThat/PictureThat/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  PictureThat
+//
+//  Created by 이민섭 on 2022/12/28.
+//
+
+import UIKit
+import WebKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    @IBOutlet weak var mainWebView: WKWebView!
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+            
+        self.setInitialUi()
+        
+    }
+    
+        
+    
+}
+
+// MARK: - Methods
+extension MainViewController{
+    
+    func setInitialUi(){
+        
+        //mainWebview set
+        let urlString = "https://kingssajonjaltwo.github.io/practice_MVP/JonZalSop/"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let urlReq = URLRequest(url: url)
+        mainWebView.load(urlReq)
+        
+    }
+    
+}
