c05a8928e0c1aa1c0a37d1a4d15aee49d9df020a
minsop <minsoplee2015@gmail.com>
2022-12-28 15:59:56 +0900

    initial commit
    project setting
    add .gitignore

---
+++ PictureThat/PictureThat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureThat
+//
+//  Created by 이민섭 on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
