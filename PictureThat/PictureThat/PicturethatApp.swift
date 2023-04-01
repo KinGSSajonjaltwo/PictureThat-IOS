9f03a6a37e0e472e77ef6bed478ea89bc3f999a6
mininim <minsoplee2015@gmail.com>
2023-04-01 19:56:52 +0900

    스유로 플젝 변경

---
+++ PictureThat/PictureThat/PicturethatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicturethatApp.swift
+//  Picturethat
+//
+//  Created by Eric Lee on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct PicturethatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
