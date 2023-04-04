fc3247d31d01cd47599339d02d6cd288c55f409d
mininim <minsoplee2015@gmail.com>
2023-04-04 21:31:25 +0900

    [FIX] Card 초기화 오타 수정

--- PictureThat/PictureThat/Model/ModelData.swift
+++ PictureThat/PictureThat/Model/ModelData.swift
@@ -10,10 +10,10 @@ import Foundation
 class ModelData {
 
     var cardDeck: [Card] = [
-        Card(name: "1", imgUrl: "1 url"),
-        Card(name: "2", imgUrl: "1 url"),
-        Card(name: "3", imgUrl: "1 url"),
-        Card(name: "4", imgUrl: "1 url")
+        Card(name: "1", imgURL: "1 url"),
+        Card(name: "2", imgURL: "1 url"),
+        Card(name: "3", imgURL: "1 url"),
+        Card(name: "4", imgURL: "1 url")
     ]
     
 }
