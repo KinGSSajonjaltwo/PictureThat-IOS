a89994a1da61c1fefa6f1b6c389679c2c8cb69c6
mininim <minsoplee2015@gmail.com>
2023-04-18 20:31:20 +0900

    [FEAT] #36 - ver1Length를 사용해서 랜덤 인덱스 추출
    랜덤 추출
    중복 검사

--- Picturethat/Picturethat/Manager/Network.swift
+++ Picturethat/Picturethat/Manager/Network.swift
@@ -7,28 +7,40 @@
 import Foundation
 import FirebaseDatabase
 
-final class Network {
+final class Network{
     
-    @Published var cards: [Card] = [Card(name: "Test1", imgURL: "test1")]
+    static let shared = Network()
+    
+    // MARK: - Properties
+    var cards: [Card] = []
+    private var indexHistory: Set<Int> = Set<Int>()
+    private var ver1Length: Int = 30
+    
     private let ref = Database.database().reference()
 
     // MARK: - GET INDEX OF CARD
-    func getIndexes(count: Int) -> [String] {
-        var result: [String] = []
+    func getIndexes(count: Int) -> Set<Int> {
+        var result: Set<Int> = Set<Int>()
         
-        for index in 1...count{
-            result.append("\(index)")
+        if indexHistory.count > 30 {
+            indexHistory = []
+        }
+        
+        while result.count < count{
+            let randomIndex = Int.random(in: 1...ver1Length)
+            result.insert(randomIndex)
+            indexHistory.insert(randomIndex)
         }
         
         return result
     }
     
     // MARK: - GET a set number of cards
-    func getCards(count: Int)-> Void{
+    func getCards(count: Int) -> Void{
         let indexes = getIndexes(count: count)
         
         for i in indexes {
-            ref.child(i).observeSingleEvent(of: .value) { snapshot in
+            ref.child("\(i)").observeSingleEvent(of: .value) { snapshot in
                 guard let card = snapshot.value else { return }
                 do {
                     let jsonData = try JSONSerialization.data(withJSONObject: card, options: [])
@@ -42,9 +54,19 @@ final class Network {
             }
             
         }
-
         
     }
     
+    // MARK: - GET Ver1 Length
+    func getVer1Length() -> Void {
+        
+        ref.child("ver1Length").observeSingleEvent(of: .value) { snapshot in
+            guard let returnVer1Length = snapshot.value as? Int else {return}
+            self.ver1Length = returnVer1Length
+        }
+        
+    }
+    
+                                                   
 }
-
+                                                   
