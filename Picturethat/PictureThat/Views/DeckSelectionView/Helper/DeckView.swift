83d84a05ff9c8ec10ac7da01698fdf919ec6c862
mininim <minsoplee2015@gmail.com>
2023-05-06 11:45:37 +0900

    [FEAT] #43 - DeckView deck 프로퍼티 추가
    프로퍼티 변경
    deckImage -> deck

--- Picturethat/PictureThat/Views/DeckSelectionView/Helper/DeckView.swift
+++ Picturethat/PictureThat/Views/DeckSelectionView/Helper/DeckView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct DeckView: View {
     var size: CGSize
-    var deckImage: String
+    var deck: Deck
     let cornerRadius: CGFloat = 15
     let borderWidth: CGFloat = 4
     
-    init(width: CGFloat = 300, height: CGFloat = 400, deckImage: String = "img_heartDeckThumbnail") {
+    init(width: CGFloat = 300, height: CGFloat = 400, deck: Deck = Deck(deckImage: "img_heartDeckThumbnail")) {
         self.size = CGSize(width: width, height: height)
-        self.deckImage = deckImage
+        self.deck = deck
     }
     
     
@@ -28,7 +28,7 @@ struct DeckView: View {
                 .frame(width: size.width, height: size.height)
                 .shadow(color: Color.deckShadowColor, radius: 2, x: 0, y: 2)
             
-            Image(self.deckImage)
+            Image(self.deck.deckImage)
                 .resizable()
                 .frame(width: size.width - borderWidth*2, height: size.height - borderWidth*2)
                 .aspectRatio(contentMode: .fill)
