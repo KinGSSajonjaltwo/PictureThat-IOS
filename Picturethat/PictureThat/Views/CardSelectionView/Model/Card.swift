9c1482d3df22386401d71485fab050f093363fc2
mininim <minsoplee2015@gmail.com>
2023-05-25 22:10:43 +0900

    [FEAT/#55] Card 모델 구현

---
+++ Picturethat/PictureThat/Views/CardSelectionView/Model/Card.swift
@@ -0,0 +1,24 @@
+//
+//  Card.swift
+//  Picturethat
+//
+//  Created by Eric Lee on 2023/05/24.
+//
+
+import SwiftUI
+
+struct Card: Identifiable, Codable{
+    
+    var deckID: String
+    var id: String
+    var title: String
+    var imageURL: String
+    var source: String
+    
+}
+
+extension Card{
+    
+    static var sampleCard1 = Card(deckID: "소노공", id: "손오공1", title: "#두등등장", imageURL: "https://firebasestorage.googleapis.com/v0/b/picturethat-9f8ae.appspot.com/o/Decks%2F%EC%86%90%EC%98%A4%EA%B3%B5%2F%E1%84%89%E1%85%A9%E1%84%82%E1%85%A9%E1%84%80%E1%85%A9%E1%86%BC1.png?alt=media&token=85cd2400-06ab-467b-abb9-4959a71a437f", source: "@seventeen_17_n")
+    
+}
