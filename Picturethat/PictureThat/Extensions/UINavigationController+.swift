fcd18d02e19a1cc8ae243d7cf4823565db165ad6
mininim <minsoplee2015@gmail.com>
2023-06-01 21:43:43 +0900

    [FIX] navigationBarBackButtonHidden swipe back gesture 오류

---
+++ Picturethat/PictureThat/Extensions/UINavigationController+.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+.swift
+//  Picturethat
+//
+//  Created by Eric Lee on 2023/06/01.
+//
+
+import SwiftUI
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
